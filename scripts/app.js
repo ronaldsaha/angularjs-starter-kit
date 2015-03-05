ee8d3e818c431852c60a9873cbc06be8b7d043c3
ronaldsaha <ronald.saha@adpeople.com>
2015-03-05 06:59:49 +0600

    setting up requirejs to work..

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,5 @@
-/**
- * loads sub modules and wraps them up into the main module
- * this should be used for top-level module definitions only
- */
 define([
-    'angular',
-    'angular-route',
+    'angular','angular-route',
     './controllers/index',
     './directives/index',
     './filters/index',
--- scripts/routes.js
+++ scripts/routes.js
@@ -2,12 +2,12 @@ define(['./app'], function (app) {
     'use strict';
     return app.config(['$routeProvider', function ($routeProvider) {
         $routeProvider.when('/view1', {
-            templateUrl: 'partials/partial1.html',
+            templateUrl: 'scripts/views/partial1.html',
             controller: 'MyCtrl1'
         });
 
         $routeProvider.when('/view2', {
-            templateUrl: 'partials/partial2.html',
+            templateUrl: 'scripts/views/partial2.html',
             controller: 'MyCtrl2'
         });
 
